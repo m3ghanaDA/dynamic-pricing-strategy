6a0fd8ba79f135500a0e42c5ef7493b62cf4b90c
m3ghanaDA <meghanada99@gmail.com>
2026-07-09 19:20:42 +0530

    initial update

---
+++ app.py
@@ -0,0 +1,11 @@
+import streamlit as st
+
+st.set_page_config(
+    page_title="Dynamic Pricing Dashboard",
+    page_icon="🚖",
+    layout="wide"
+)
+
+st.title("🚖 Dynamic Pricing Strategy Dashboard")
+
+st.success("Streamlit is working successfully!")
